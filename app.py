d71702acc596b902f5db122a809efb4fd7a8eee5
Shreethaar <shreethaara380@gmail.com>
2024-06-28 10:50:53 +0000

    static && template for flask

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import sklearn
+import pickle
+import pandas as pd
+
+
 app=Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello,World!'
+#cross_origin()
+def home():
+    return render_template("home.html")
+
+@app.route("/predict",methods=["GET", "POST"])
+#cross_origin()
+def predict():
+    if request.method == "POST":
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
